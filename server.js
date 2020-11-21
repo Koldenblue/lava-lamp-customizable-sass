5308f04f3d380ca260a8f9c7269fc08ee750e355
Kevin Miller <kmillergit@outlook.com>
2020-11-20 17:43:24 -0800

    homepage style, server setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(
+    "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+    PORT,
+    PORT
+  );
+});
