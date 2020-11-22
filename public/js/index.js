496cb2d50512261b148b4e215a76953100f9a8cb
Kevin Miller <kmillergit@outlook.com>
2020-11-21 19:54:00 -0800

    styled 3 lamps, added lamp switch

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,17 @@
-$document.ready(() => {
-  document.addEventListener('resize', )
+$(document).ready(() => {
+  let lamp = $("#lamp-section")
+
+  const colorChange = () => {
+    if (lamp.attr('class') === 'purple-lamp') {
+      lamp.attr('class', 'green-lamp');
+    }
+    else if (lamp.attr('class') === 'green-lamp') {
+      lamp.attr('class', 'blue-lamp');
+    }
+    else if (lamp.attr('class') === 'blue-lamp') {
+      lamp.attr('class', 'purple-lamp');
+    }
+  }
+  
+  $("#color-change-btn").on('click', colorChange);
 })
