67416848754b47cb4aa3c5c42d29e9a9790d02b1
Kevin Miller <kmillergit@outlook.com>
2020-11-24 11:00:49 -0800

    added info section, heroku deploy

--- public/js/index.js
+++ public/js/index.js
@@ -5,9 +5,12 @@ $(document).ready(() => {
   let gifBtn = $("#lava-gif-button");
   let gifDiv = $("#lava-gif");
   let gifDiv2 = $("#lava-gif-2");
-
+  let gifData = [];
+  // let windowHeight;
+  // let elements;
 
   console.log(window.location.pathname)
+
   const colorChange = () => {
     if (lamp.attr('class') === 'purple-lamp') {
       lamp.attr('class', 'green-lamp');
@@ -23,6 +26,7 @@ $(document).ready(() => {
   const getGif = () => {
     $.get('/api/lavagif').then(data => {
       console.log(data);
+      let gifData = data.data;
       let gif = data.data[0].images.original.url;
       let gif2 = data.data[1].images.original.url;
       gifDiv.html(`<img src=${gif} class='lava-gif' alt='lava lamp gif'>`)
@@ -30,7 +34,27 @@ $(document).ready(() => {
     })
   }
 
-  getGif();
+  // const handleResize = () => {
+  //   windowHeight = window.innerHeight;
+  // }
+
+  // function checkPosition() {
+  //   for (var i = 0; i < elements.length; i++) {
+  //     var element = elements[i];
+  //     var positionFromTop = elements[i].getBoundingClientRect().top;
+
+  //     if (positionFromTop - windowHeight <= 0) {
+  //       element.classList.add('fade-in-element');
+  //       element.classList.remove('hidden');
+  //     }
+  //   }
+  // }
+
+  if (window.location.pathname === '/pictures') {
+    getGif();
+    // window.addEventListener('scroll', checkPosition);
+    // window.addEventListener('resize', handleResize);
+  }
 
   $("#color-change-btn").on('click', colorChange);
   gifBtn.on("click", getGif);
