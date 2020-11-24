82bd198dcaab6afad551b5eb30369f4e17332c16
Kevin Miller <kmillergit@outlook.com>
2020-11-23 21:24:33 -0800

    display and center gifs upon page load

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,8 @@ $(document).ready(() => {
   let lamp = $("#lamp-section")
   let gifBtn = $("#lava-gif-button");
   let gifDiv = $("#lava-gif");
+  let gifDiv2 = $("#lava-gif-2");
+
 
   console.log(window.location.pathname)
   const colorChange = () => {
@@ -22,11 +24,13 @@ $(document).ready(() => {
     $.get('/api/lavagif').then(data => {
       console.log(data);
       let gif = data.data[0].images.original.url;
-      gifDiv.html(`<img src=${gif}>`)
+      let gif2 = data.data[1].images.original.url;
+      gifDiv.html(`<img src=${gif} class='lava-gif' alt='lava lamp gif'>`)
+      gifDiv2.html(`<img src=${gif2} class='lava-gif' alt='lava lamp gif'>`)
     })
-
-
   }
+
+  getGif();
 
   $("#color-change-btn").on('click', colorChange);
   gifBtn.on("click", getGif);
